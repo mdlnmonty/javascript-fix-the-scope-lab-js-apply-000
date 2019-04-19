f96a88d423b062c0394320140d41e076a3b784d8
Madeline Montgomery <mdlnmontgomery@gmail.com>
2019-04-19 02:45:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  const two = 2;
-  return n + two;
+  const two = 2
+  return n + two
 }
 
 var funkyFunction = function() {
